4b599feaed603c7d1ff50e4a16d9dbfffa032e75
AlenaKharytonchyk <alena.kharytonchyk@outlook.com>
2019-03-29 23:13:11 +0300

    chore: add gulp-eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
-const { src, dest, parallel, series } = require('gulp');
+const {
+  src, dest, parallel, series
+} = require('gulp');
 const less = require('gulp-less');
 const postcss = require('gulp-postcss');
 const minifyCSS = require('gulp-csso');
 const concat = require('gulp-concat');
 const inject = require('gulp-inject');
-
+const eslint = require('gulp-eslint');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
 function html() {
   const target = src('./src/index.html');
-  const sources = src(['./build/js/*.js', './build/css/*.css'], {read: false});
- 
+  const sources = src(['./build/js/*.js', './build/css/*.css'], { read: false });
+
   return target.pipe(inject(sources))
     .pipe(dest('./build'));
 }
@@ -19,18 +21,20 @@ function css() {
   const processors = [
     autoprefixer,
     cssnano
-];
+  ];
   return src('src/*.less')
     .pipe(less())
     .pipe(postcss(processors))
     .pipe(minifyCSS())
-    .pipe(dest('build/css'))
+    .pipe(dest('build/css'));
 }
 
 function js() {
   return src('src/*.js', { sourcemaps: true })
+    .pipe(eslint())
+    .pipe(eslint.format())
     .pipe(concat('app.min.js'))
-    .pipe(dest('build/js', { sourcemaps: true }))
+    .pipe(dest('build/js', { sourcemaps: true }));
 }
 
 exports.js = js;
