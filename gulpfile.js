ffb3fd891ac19c2d205c8147011fadb319183472
AlenaKharytonchyk <alena.kharytonchyk@outlook.com>
2019-03-30 11:40:29 +0300

    chore: add gulp web-server

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const {
-  src, dest, parallel, series
+  src, dest, parallel, series, watch
 } = require('gulp');
 const less = require('gulp-less');
 const postcss = require('gulp-postcss');
@@ -9,24 +9,35 @@ const inject = require('gulp-inject');
 const eslint = require('gulp-eslint');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const connect = require('gulp-connect');
 
 function html() {
   const target = src('./src/index.html');
-  const sources = src(['./build/js/*.js', './build/css/*.css'], { read: false });
+  const sources = src(['./build/js/*.js', './build/css/*.css'], {
+    read: false
+  });
 
-  return target.pipe(inject(sources))
-    .pipe(dest('./build'));
+  return target
+    .pipe(
+      inject(sources, {
+        addRootSlash: false,
+        ignorePath: '/build/'
+      })
+    )
+    .pipe(dest('./build'))
+    .pipe(connect.reload());
 }
 function css() {
-  const processors = [
-    autoprefixer,
-    cssnano
-  ];
+  const processors = [autoprefixer, cssnano];
   return src('src/*.less')
     .pipe(less())
     .pipe(postcss(processors))
     .pipe(minifyCSS())
-    .pipe(dest('build/css'));
+    .pipe(dest('build/css'))
+    .pipe(connect.reload());
+}
+function webserver() {
+  return connect.server({ livereload: true, root: ['build'] });
 }
 
 function js() {
@@ -34,10 +45,14 @@ function js() {
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(concat('app.min.js'))
-    .pipe(dest('build/js', { sourcemaps: true }));
+    .pipe(dest('build/js', { sourcemaps: true }))
+    .pipe(connect.reload());
 }
-
-exports.js = js;
-exports.css = css;
-exports.html = html;
+function watchTask(cb) {
+  watch('src/*.less', css);
+  watch('src/*.js', js);
+  watch('src/index.html', html);
+  cb();
+}
+exports.watch = series(parallel(css, js), html, parallel(webserver, watchTask));
 exports.default = series(parallel(css, js), html);
