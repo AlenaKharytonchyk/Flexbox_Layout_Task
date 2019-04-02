e9a2a176d1b15ba6ad6961f9f07a047fa646008b
AlenaKharytonchyk <alena.kharytonchyk@outlook.com>
2019-04-02 11:07:20 +0300

    feat: opportunity to add text to chat

--- src/chat.js
+++ src/chat.js
@@ -1,16 +1,17 @@
-$('#start-chat').click(function startChat() { alert("Let's start!"); });
+$('#start-chat').click(function startChat() {
+  alert("Let's start!");
+});
 
 const email = localStorage.getItem('userEmail');
 $('.user-email').text(email);
 
-$.getJSON('chat-message.json')
-  .done(function json(data) {
+$.getJSON('chat-message.json').done(function json(data) {
   // const elementsArr = [];
-    $.each(data, function newChatElement(key, value) {
-      $('<div/>', {
-        class: 'chat-section-card',
-        id: `message-${key}`,
-        html: `
+  $.each(data, function newChatElement(key, value) {
+    $('<div/>', {
+      class: 'chat-section-card',
+      id: `message-${key}`,
+      html: `
       <div>
                   <img src="./assets/speak.png" class="communities-image" />
                 </div>
@@ -24,6 +25,37 @@ $.getJSON('chat-message.json')
                     </p>
                   </div>
                 </div>`
-      }).appendTo('main.chat-section-field');
-    });
+    }).appendTo('main.chat-section-field');
   });
+});
+
+$('#chat-message-text').keydown(function keydown(event) {
+  let message = $('textarea').val();
+  if (event.keyCode === 13) {
+    if (message === '') {
+      alert('Enter Some Text In Textarea');
+    } else {
+      (function newMessageInChat() {
+        $('<div/>', {
+          class: 'chat-section-card',
+          html: `
+          <div>
+                      <img src="./assets/speak.png" class="communities-image" />
+                    </div>
+                    <div class="chat-section-card-text">
+                      <span class="communities-name user-email">
+                      ${email}
+                      </span>
+                      <div class="chat-section-message">
+                        <p class="user-message">
+                        ${message}
+                        </p>
+                      </div>
+                    </div>`
+        }).appendTo('main.chat-section-field');
+      }());
+    }
+    $('textarea').val('');
+    return false;
+  } return true;
+});
